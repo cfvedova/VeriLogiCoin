3a7ba8eddc3e05a17d40d5cf476babfc4d16b2d6
JacobChmura <jacob.chmura@mail.utoronto.ca>
2019-03-16 14:51:10 -0400

    Datapath for bargraphs complete.

--- Visuals/bar_graph_datapath.v
+++ Visuals/bar_graph_datapath.v
@@ -1,39 +1,28 @@
-module bar_graph_datapath(clk, resetn, start_x, start_y, enable, coord_x, coord_y);
+module bar_graph_datapath(clk, resetn, start_x, start_y, graph_height, enable, coord_x, coord_y);
     input clk
     input resetn
 	input [9:0] start_x // 0 -> 640
 	input [8:0] start_y // 0 -> 480
-	input [2:0] color_in // VGA color encodings
 	input enable  
+	input [7:0] graph_height; // 0 -> 200
     output [9:0] x_coord // X value to plot
 	output [8:0] y_coord // Y value to plot
-	output [2:0] color_out
 
     
     // input registers
-    reg [7:0] x;
-	reg [6:0] y;
-	reg [2:0] color;
+    reg [9:0] x;
+	reg [8:0] y;
 
-    // output of the alu
-	reg [1:0] offset_x;
-	reg [1:0] offset_y;
+    // store iteration count
+	reg [4:0] offset_x; //Bar graph is 32 pixels wide
+	reg [7:0] offset_y; // Bar graph is at most 200 pixels tall
 	wire y_enable;
     
     // Registers x, y with respective input logic
     always @ (posedge clk) begin
         if (!resetn) begin
-            x <= 8'd0;
-			y <= 7'd0;
-			color <= 3'd0;
-        end
-        else begin
-            if (ld_X)
-                x <= {1'd0, position}; // load alu_out if load_alu_out signal is high, otherwise load from data_in
-            if (ld_Y)
-                y <= position; // load alu_out if load_alu_out signal is high, otherwise load from data_in
-			if (ld_C)
-				color <= color_in;
+            x <= 10'b0;
+			y <= 9'b0;
         end
     end
 
@@ -41,34 +30,29 @@ module bar_graph_datapath(clk, resetn, start_x, start_y, enable, coord_x, coord_
     // counter for x
 	always @(posedge clk) begin
 		if (!resetn)
-			offset_x <= 2'b00;
-		else if (x_enable) begin
-			if (offset_x == 2'b11)
-				offset_x <= 2'b00;
-			else begin
+			offset_x <= 5'b0;
+		else if (enable) begin
+			if (offset_x != 5'b11111) //Width is at most 32 pixels
 				offset_x <= offset_x + 1'b1;
-			end
+			else 
+				offset_x <= 5'b0;
 		end
 	end
 	
-	assign y_enable = (offset_x == 2'b11) ? 1 : 0;
+	assign y_enable = (offset_x == 5'b11111) ? 1 : 0;
 	
 	// counter for y
 	always @(posedge clk) begin
 		if (!resetn)
-			offset_y <= 2'b00;
-		else if (x_enable && y_enable) begin
-			if (offset_y != 2'b11)
+			offset_y <= 5'b0;
+		else if (enable && y_enable) begin
+			if (offset_y != 8b'11001000) // Height is at most 200 pixels
 				offset_y <= offset_y + 1'b1;
 			else 
-				offset_y <= 2'b00;
+				offset_y <= 8'b00000000;
 		end
 	end
-	
 
 	assign x_coord = x + offset_x;
-	assign y_coord = y + offset_y;
-	assign color_out = color;
-	
-    
+	assign y_coord = y + offset_y; 
 endmodule
