b430e1e6721720354b50baad0b5edd4072057dc8
cfvedova <42545401+cfvedova@users.noreply.github.com>
2019-03-31 22:17:29 -0400

    Changed ram for block storage

--- Memory/RAM/ram_real.v
+++ Memory/RAM/ram_real.v
@@ -1,16 +1,26 @@
 module ram(clock, access_type, data_in, wren, result, resetn);
 	input clock, resetn;
-	input [0:0] access_type; //0: player values, 1: blockchain
+	input access_type; //0: player values, 1: blockchain
 	input [47:0] data_in;
 	input wren; //Write or read
-	output reg [47:0] result;
+	output [47:0] result;
 	
+	reg [47:0] transaction_data;
+	reg [7:0] previous_hash;
 	
 	always @(posedge clock) begin
 		if (!resetn)
-			result <= 48'b0;
-		if (wren) begin
-			result <= data_in;
+			transaction_data <= 48'b0;
+			previous_hash <= 8'b11111111;
+		else if (wren) begin
+			if (access_type) begin
+				previous_hash <= data_in[7:0];
+			end
+			else begin
+				transaction_data <= data_in;
+			end
 		end
 	end
+	
+	assign result = (access_type) ? {40'b0, previous_hash} : transaction_data;
 endmodule
